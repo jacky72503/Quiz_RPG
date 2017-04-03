2ab8d259a5a55f06a9fcaaf66c7e27daf097d273
jacky72503 <jacky72503@gmail.com>
2017-04-03 19:34:28 +0800

    use http to connect server

--- app/src/main/java/com/jacky72503/qarpg/LoginActivity.java
+++ app/src/main/java/com/jacky72503/qarpg/LoginActivity.java
@@ -2,42 +2,73 @@ package com.jacky72503.qarpg;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import butterknife.ButterKnife;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import DBConnect.DBConnector;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class LoginActivity extends AppCompatActivity {
+    /*public static void map_put(Map map){
+        map.put();
+    }*/
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
-    @BindView(R.id.input_email) EditText _emailText;
-    @BindView(R.id.input_password) EditText _passwordText;
-    @BindView(R.id.btn_login) Button _loginButton;
-    @BindView(R.id.link_signup) TextView _signupLink;
+    public static DBConnector DBConnector;
+    private static Thread thread;
+    private static Map<String,String> map = new HashMap<>();
+    private static StringRequest getRequest;
+    @BindView(R.id.input_email)
+    EditText emailText;
+    @BindView(R.id.input_password)
+    EditText passwordText;
+    @BindView(R.id.btn_login)
+    Button loginButton;
+    @BindView(R.id.link_signup)
+    TextView signupLink;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-
-        _loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                map.put("account",emailText.getText().toString());
+                map.put("password",passwordText.getText().toString());
+                DBConnector=new DBConnector("http://1.34.199.74:80/login.php",map);
                 login();
             }
         });
 
-        _signupLink.setOnClickListener(new View.OnClickListener() {
+        signupLink.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -46,6 +77,9 @@ public class LoginActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_SIGNUP);
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void login() {
@@ -56,27 +90,47 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        _loginButton.setEnabled(false);
+        loginButton.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
         progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
+        progressDialog.setMessage(getResources().getString(R.string.authenticating));
         progressDialog.show();
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        /*Runnable mutiThread = new Runnable(){
+            public void run(){
+                //DBConnector.sendHttpURLConnectionPOST();
+                onLoginSuccess();
+                // onLoginFailed();
+                progressDialog.dismiss();
+            }
+        };
+        Thread thread = new Thread(mutiThread);
+        thread.start();*/
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                DBConnector.sendHttpURLConnectionPOST();
+            }
+        });
+        thread.start();
+        //onLoginSuccess();
         // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
+        /*new Handler().postDelayed(
                 new Runnable() {
                     public void run() {
+                        //JSONObject result=DBConnect.getJSONFromUrl("http://localhost:80/login.php");
+                        //Toast.makeText(getBaseContext(), result.length()+"", Toast.LENGTH_LONG).show();
                         // On complete call either onLoginSuccess or onLoginFailed
+                        //DBConnector.sendHttpURLConnectionPOST();
                         onLoginSuccess();
                         // onLoginFailed();
                         progressDialog.dismiss();
                     }
-                }, 3000);
+                },
+                3000);*/
     }
 
 
@@ -99,36 +153,71 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginSuccess() {
-        _loginButton.setEnabled(true);
-        finish();
+        loginButton.setEnabled(true);
+        //finish();
     }
 
     public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _loginButton.setEnabled(true);
+        Toast.makeText(getBaseContext(), getResources().getString(R.string.login_failed), Toast.LENGTH_LONG).show();
+        loginButton.setEnabled(true);
     }
 
     public boolean validate() {
         boolean valid = true;
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError(getResources().getString(R.string.valid_email_address));
             valid = false;
         } else {
-            _emailText.setError(null);
+            emailText.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            passwordText.setError(getResources().getString(R.string.password_4_to_10));
             valid = false;
         } else {
-            _passwordText.setError(null);
+            passwordText.setError(null);
         }
 
         return valid;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Login Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
